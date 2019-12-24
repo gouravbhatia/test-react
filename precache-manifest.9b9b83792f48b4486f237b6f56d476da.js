14be03df272f4b5a17348e27c36cec6d016c8e90
Gourav Bhatia <gourav.bhatia639@gmail.com>
2019-12-25 00:05:42 +0530

    Updates

---
+++ precache-manifest.9b9b83792f48b4486f237b6f56d476da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5de19ca4c1b989e3eb0d372eb206fdd",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "91ea020711a1b0390ab9",
+    "url": "/test-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a0239b6f41000d1716a6",
+    "url": "/test-react/static/js/2.5c197d77.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react/static/js/2.5c197d77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91ea020711a1b0390ab9",
+    "url": "/test-react/static/js/main.f5ff47b5.chunk.js"
+  },
+  {
+    "revision": "1c56048d55b44e817f6d",
+    "url": "/test-react/static/js/runtime-main.3528860c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
